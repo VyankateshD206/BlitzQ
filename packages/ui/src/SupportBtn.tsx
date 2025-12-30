5b6fd2932daad0bce523aa30e86935030abd0efa
Vyankatesh Deshpande <b23cs1079@iitj.ac.in>
2025-12-30 21:40:46 +0530

    added support feature

--- packages/ui/src/SupportBtn.tsx
+++ packages/ui/src/SupportBtn.tsx
@@ -4,7 +4,7 @@ import { MessageSquareText } from "lucide-react";
 function SupportBtn() {
   return(
     <div className="fixed bottom-5 right-5 p-3 z-20 bg-purple-600 rounded-full shadow-lg cursor-pointer hover:shadow-xl">
-        <a href="https://www.tally.so/r/wvbGv0" target="_blank">
+        <a href="https://tally.so/r/gDeEaD" target="_blank">
           <MessageSquareText
             stroke="white"
           />
